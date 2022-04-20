f8fec11290a0cf7aec157be3cd2f1a71adbdce63
Blake <blakeallen0042@gmail.com>
2022-04-19 19:52:36 -0600

    Updated Gamepiece ABC, Sphinx, Laser, Board

--- Khet.py
+++ Khet.py
@@ -14,12 +14,10 @@ By:  Blake McGill
 Goals:
 Make grid string generator that generates an X by Y size board with each cell being able to hold a single character
     -equal spacing characters required
-Make functional game that two players could play via text I/O.
-
-Make GUI
+Make GUI using tkinter
 Make into game can play via HTTP PUT and GET requests
 Make both Deflexion, Khet, and Khet 2 variants
-Make basic AI (IDEA: use __ge__, __le__, etc. methods for evaluation of pieces/board)
+Make basic AI (IDEA: use __ge__, __le__, etc. methods for evaluation of pieces/board state)
 Make error wrapper and handler class
 Make Khet-specific error types
 Log:
@@ -28,22 +26,10 @@ Log:
 -03/08/2021//Updated comments, docstrings, added them where possible, continuing gamepiece class development.
 -02/20/2022//Replaced method for reflection of laser when it interacts with gamepiece (new-> ReflectMatrix)
 '''
-from collections import deque
 from abc import ABC, abstractmethod
 from dataclasses import dataclass, field
-from msilib.schema import Error
-from tempfile import TemporaryFile
-from xmlrpc.client import Boolean
+from urllib.parse import ParseResultBytes
 
-NORTH = 0
-EAST = 1
-SOUTH = 2
-WEST = 3
-DIRECTIONS = (NORTH, EAST, SOUTH, WEST)
-MOVE_MATRIX = ([0,1],[1,0],[0,-1],[-1,0])
-
-OSIRIS = 'Red'
-IMHOTEP = 'Silver'
 def rotate(self, cw: bool = True):
     '''
     Rotate gamepiece 1/4 turn (90°) clockwise or counterclockwise using a deque.
@@ -73,38 +59,34 @@ def move(self, direction):
 class Board:
     #10x8
     #holds state of board, including piece position, player piece count, tile type
-    def __init__(self):
-        pass
-
-class Laser:
-    '''
-    Class for laser operation. Uses Doubly-Linked List to 'build' laser.
-    '''
-    def __init__(self):
-        pass
-    
-    def __repr__(self):
-        pass
+    def __init__(self, rank = 8, file = 10):
+        self._MAX_RANK = max(rank)
+        self._MAX_FILE = max(file)
+        self._board_state = dict()
+        for rank in range(rank):
+            for file in range(file):
+                self._board_state[(rank, file)] = None
 
 class Gamepiece(ABC):
     '''
-    Abstract base dataclass for gamepieces. Stores player, movement, and reflection data
+    Abstract base dataclass for gamepieces. Stores player affiliation, movement 
+    ruleset, and reflection data.
     '''
     @abstractmethod
-    def __init__(self, player : str, reflect_state = None):
+    def __init__(self, player : str):
         self.player = player
         self._can_move = True
         self._can_rotate = True
-        self._reflect_state = list(reflect_state) if reflect_state else [4]*4
+        self._reflect_state = None
         pass
 
     @property
     def name(self):
-        '''
-        Property returning name of Gamepiece using built-in class/name methods
-        Big O: O(1)
-        '''
         return self.__class__.__name__
+
+    @property
+    def player(self):
+        return self.player
 
     @property
     def can_move(self):
@@ -123,47 +105,62 @@ class Gamepiece(ABC):
         assert(len(state)==4)
         self._reflect_state = state
 
+    def __call__(self, rank: int, file: int) -> str:
+        '''
+        Object can be called while providing a location to store it internally within the Gamepiece.
+        Will be useful in AI features.
+        '''
+        self.location = (rank, file)
+        return f'Location: rank {rank}, file {file} stored in {self.name} data.'
+
+    def __str__(self) -> str:
+        return self.__class__.__name__
+
 class Sphinx(Gamepiece):
     '''
     Gamepiece child class for Sphinx Gamepiece.
     Sphinx is stationary, and has limited rotation capabilities.
     '''
-    _DEFAULT_LOCATION = dict({OSIRIS: [9,0], IMHOTEP: [0,7]}) #default locations for each player's sphinx
+    class Laser:
+        '''
+        Class for laser operation.
+        '''
+        #Matrix to use in beam calculations: Direction : Sphinx Location, Offset to Next Beam Location
+        LOCATION_DIRECTION_MATRIX = {
+            'N':((7, 9), (-1, 0)),
+            'E':((0, 0), (0,  1)),
+            'S':((0, 0), (1,  0)),
+            'W':((7, 9), (0, -1))
+            }
 
-    def __init__(self, faction='Red', *location):
+        def __init__(self, direction):
+            self._initial_direction = direction
+        
+        def beam(self, board_state : dict):
+            '''
+            Uses board_state and initial direction to generate laserbeam path on board.
+            Returns location of hit gamepiece, or wall indication
+            '''
+            for rank in range()
+            pass
+
+    VALID_STATE = ('Block','Block','Block','Block') #Only valid reflect state for Sphinx
+    VALID_FACES_FOR = {'Red': ('S','E'), 'Silver': ('N','W')}
+    def __init__(self, player : str, face = None):
         '''
         Initialize gamepiece for Sphinx. Ignores location parameter.
         Sphinx placed based on faction.
         '''
-        try:
-            super().__init__(self._DEFAULT_LOCATION[faction], faction) #run gamepiece class init with location and faction
-            #reflect matrix all zeroes
-            if self.faction == IMHOTEP: #if faction is red
-            #matrix has [1, 0, 0, 0] NORTH
-            else: #matrix has [0, 0, 1, 0] SOUTH
-        except:
-            print('Error in Sphinx initialization!')
+        super.__init__(player)
+        default_face = self.VALID_FACES_FOR[player][0] #default face for direction of laser
+        self._face = face if face in self.VALID_FACES_FOR[player] else default_face
+        self._laser = self.Laser(self._face)
+        self._can_move = False
+        self._reflect_state = self.VALID_STATE
 
-    
-    def move(self, *direction):
-        '''
-        Overwrites Gamepiece move method to indicate Sphinxes can't move.
-        '''
-        print(f'Can\'t move {direction}; Reason: {self.name} gamepieces can\'t move.')
-
-    def rotate(self):
-        '''
-        Overwritten rotate class that restricts movement of Sphinx to never aim laser at board edge.
-        '''
-        if isinstance(self.body['Top'], Laser) or isinstance(self.body['Bottom'], Laser): #if laser is facing up or down
-            super().rotate(False) #call Gamepiece method for counterclockwise rotation
-        else: super().rotate(True) #else call for clockwise rotation
-        for side in self._SIDES: #for each side in lookup list
-            if isinstance(self.body[side], Laser): #if side has laser
-                return self.body[side] #return side with laser on it
-    
-    def to_Duat(self):
-        print('Sphinxes never die.')
+    @property
+    def laser(self):
+        return self._laser
 
 class Pharaoh(Gamepiece):
     def __init__(self):
@@ -200,7 +197,6 @@ class Curator():
     def __init__(self):
         self.players = Player(), Player()
         self.board = Board()
-        self.
         pass
 
 '''
@@ -212,7 +208,8 @@ def debug():
     print(f'Rotate() return with Argument: \'None (default=clockwise)\':\n     {test_gamepiece.rotate()}')
     print(f'Move() return with Argument: \'Down\':\n     {test_gamepiece.move("Down")}')
 
-if __name__ == '__main__': debug()
+if __name__ == '__main__': 
+    print()
 
 
 '''
